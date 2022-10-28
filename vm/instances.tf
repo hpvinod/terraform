b2622aec08ff9092b31cab80fd97941d4df4cac5
Vinod Kumar <hp.vinod@gmail.com>
2022-10-28 07:03:00 +0000

    First instance

--- vm/instances.tf
+++ vm/instances.tf
@@ -1,11 +1,18 @@
-source "google_service_account" "default" {
-  account_id   = "service_account_id"
-  display_name = "Service Account"
-}
-
 resource "google_compute_instance" "default" {
   name         = "test"
   machine_type = "e2-medium"
   zone         = "us-central1-a"
+  
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+
+  network_interface {
+    network = "testregister"
   }
 }
